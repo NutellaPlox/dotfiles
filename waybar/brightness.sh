658c4dbb91e3fe365abc7c661c3d810a6ddeb309
Harrison Hsieh <harhsieh@gmail.com>
2023-06-12 17:25:23 -0500

    Setup for 9720

--- waybar/brightness.sh
+++ waybar/brightness.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+NAME=intel_backlight
 
 if [ $1 == "increase" ]
 then
-  echo $(($(cat /sys/class/backlight/apple-panel-bl/brightness) + $2)) > /sys/class/backlight/apple-panel-bl/brightness
+  echo $(($(cat /sys/class/backlight/$NAME/brightness) + $2)) > /sys/class/backlight/$NAME/brightness
 fi
 
 if [ $1 == "decrease" ]
 then
-  echo $(($(cat /sys/class/backlight/apple-panel-bl/brightness) - $2)) > /sys/class/backlight/apple-panel-bl/brightness
+  echo $(($(cat /sys/class/backlight/$NAME/brightness) - $2)) > /sys/class/backlight/$NAME/brightness
 fi
